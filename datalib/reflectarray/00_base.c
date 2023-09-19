ef51249371935fa79ed0372a1bda3d21317fbd64
kenshi0902 <kenshi0902@gmail.com>
2023-09-19 17:10:36 +0900

    メタ表面のグリッドとパッチを描画す

--- datalib/reflectarray/00_base.c
+++ datalib/reflectarray/00_base.c
@@ -11,8 +11,9 @@
 int main(void)
 {
 	const int nxarray = 5;
-	const int nyarray = 25;
+	const int nyarray = 50;
 	const int ns = 5;
+	const int nt = 5;
 	const double freq = 14e9;
 	const double lambda = 3e8 / freq;
 	const double dx = 0.1*lambda;
@@ -24,6 +25,7 @@ int main(void)
 	const double dh = 16e-3;
 	const double margin = 0.25*lambda;
 	const double l[] = {2.13e-3, 2.04e-3, 1.91e-3, 1.75e-3, 1.57e-3};
+	const double m[] = {2e-3, 1.5e-3, 1e-3, 0.5e-3, 0.2e-3};
 
 	const double eps0 = 8.854e-12;
 	const double pi = 4 * atan(1);
@@ -38,7 +40,7 @@ int main(void)
 
 	// mesh
 
-	const double x0 = -nxarray / 2.0 * ns * dx;
+	const double x0 = -nxarray * ns * dx;
 	const double x1 = -x0;
 	const int xmesh_margin = (margin) / (0.01*lambda);
 	const int xmesh_base = (x1 - x0) / (0.005*lambda);
@@ -75,15 +77,21 @@ int main(void)
 
 	// substrate
 	ofd_geometry(2, 1, x0, x1, y0, y1, z1, z2);
-	ofd_geometry(2, 1, x0, x1, y0, y1, z1 - dh, z1);
+	// ofd_geometry(2, 1, x0, x1, y0, y1, z1 - dh, z1);
 
 	for (int i = 0; i < nxarray; i++) {
 	for (int j = 0; j < nyarray; j++) {
-		const double x = x0 + (i * ns * dx);
+		const double x = x0 + (2*i * ns * dx);
 		const double y = y0 + (j + 0.5) * dy;
-		for (int n = 0; n < ns; n++) {
-			const double xa = x + (n + 0.5) * dx;
-			ofd_geometry(1, 1, xa - l[n] /2, xa + l[n]/2, y -l[n]/2, y +l[n]/2, z2, z2);
+		const double xg = x0 + (2*i * ns * dx) + (ns * dx);
+		for (int p = 0; p < ns; p++) {
+			const double xa = x + (p + 0.5) * dx;
+			ofd_geometry(1, 1, xa - l[p] /2, xa + l[p]/2, y -l[p]/2, y +l[p]/2, z2, z2);
+		}
+		for (int g = 0; g < nt; g++) {
+			const double xb = xg + (g + 0.5) * dx;
+			//ofd_geometry(1, 1, xb, x1, -y/2, +y/2, z2, z2);
+			ofd_geometry(0, 1, xb - m[g] /2, xb + m[g]/2, y -m[g]/2, y +m[g]/2, z2, z2);
 		}
 	}
 	}
